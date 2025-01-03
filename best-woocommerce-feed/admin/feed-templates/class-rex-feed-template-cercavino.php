eb967ef502f4661c3fc47cba617df337daaf1519
Sean Murphy <sean@iamseanmurphy.com>
2025-01-03 06:17:49 +0000

    7.4.27

--- best-woocommerce-feed/admin/feed-templates/class-rex-feed-template-cercavino.php
+++ best-woocommerce-feed/admin/feed-templates/class-rex-feed-template-cercavino.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The adform Feed Template class.
+ * The Bing Feed Template class.
  *
  * @link       https://rextheme.com
  * @since      1.1.4
@@ -10,13 +10,13 @@
  */
 
 /**
- * Defines the attributes and template for adform feed.
+ * Defines the attributes and template for bing feed.
  *
  * @package    Rex_Product_Feed
- * @subpackage Rex_Product_Feed/admin/feed-templates/Rex_Feed_Template_Adform
+ * @subpackage Rex_Product_Feed/admin/feed-templates/Rex_Feed_Template_Cercavino
  * @author     RexTheme <info@rextheme.com>
  */
-class Rex_Feed_Template_Shopee extends Rex_Feed_Abstract_Template {
+class Rex_Feed_Template_Cercavino extends Rex_Feed_Abstract_Template {
 
 	/**
 	 * Define merchant's required and optional/additional attributes
@@ -26,20 +26,23 @@ class Rex_Feed_Template_Shopee extends Rex_Feed_Abstract_Template {
 	protected function init_atts() {
 		$this->attributes = array(
 			'Required Information'   => array(
-				'Product name'         => 'Product Name',
-				'SKU'                  => 'SKU',
-				'Price'                => 'Price',
-				'Stock Quantity'       => 'Stock Quantity',
-				'Main Image URL'       => 'Main Image URL',
-				'Product Description'  => 'Product Description',
-				'Shipping Information' => 'Shipping Information',
+				'product_id'   => 'Product ID',
+				'product_name' => 'Product Name',
+				'unit_price'   => 'Unit Price',
+				'product_url'  => 'Product URL',
 			),
+
 			'Additional Information' => array(
-				'Additional Image URLs' => 'Additional Image URLs',
-				'Brand'                 => 'Brand',
-				'Variations'            => 'Variations',
-				'Product Attributes'    => 'Product Attributes'
+				'image_url'   => 'Image URL',
+				'description' => 'Description',
+				'producer'    => 'Producer',
+				'wine_type'   => 'Wine Type',
+				'region'      => 'Region',
+				'appellation' => 'Appellation',
+				'vintage'     => 'Vintage',
+				'bottle_size' => 'Bottle Size',
 			),
+
 		);
 	}
 
@@ -51,7 +54,17 @@ class Rex_Feed_Template_Shopee extends Rex_Feed_Abstract_Template {
 	protected function init_default_template_mappings() {
 		$this->template_mappings = array(
 			array(
-				'attr'     => 'Product Name',
+				'attr'     => 'product_id',
+				'type'     => 'meta',
+				'meta_key' => 'id',
+				'st_value' => '',
+				'prefix'   => '',
+				'suffix'   => '',
+				'escape'   => 'default',
+				'limit'    => 0,
+			),
+			array(
+				'attr'     => 'product_name',
 				'type'     => 'meta',
 				'meta_key' => 'title',
 				'st_value' => '',
@@ -61,45 +74,26 @@ class Rex_Feed_Template_Shopee extends Rex_Feed_Abstract_Template {
 				'limit'    => 0,
 			),
 			array(
-				'attr'     => 'SKU',
+				'attr'     => 'unit_price',
 				'type'     => 'meta',
-				'meta_key' => 'sku',
+				'meta_key' => 'price',
+				'st_value' => '',
+				'prefix'   => '',
+				'suffix'   => ' ' . get_option( 'woocommerce_currency' ),
+				'escape'   => 'default',
+				'limit'    => 0,
+			),
+			array(
+				'attr'     => 'product_url',
+				'type'     => 'meta',
+				'meta_key' => 'link',
 				'st_value' => '',
 				'prefix'   => '',
 				'suffix'   => '',
 				'escape'   => 'default',
 				'limit'    => 0,
 			),
-			array(
-				'attr'     => 'Stock Quantity',
-				'type'     => 'meta',
-				'meta_key' => 'quantity',
-				'st_value' => '',
-				'prefix'   => '',
-				'suffix'   => '',
-				'escape'   => 'cdata',
-				'limit'    => 0,
-			),
-			array(
-				'attr'     => 'Main Image URL',
-				'type'     => 'meta',
-				'meta_key' => 'main_image',
-				'st_value' => '',
-				'prefix'   => '',
-				'suffix'   => '',
-				'escape'   => 'default',
-				'limit'    => 0,
-			),
-			array(
-				'attr'     => 'Product Description',
-				'type'     => 'meta',
-				'meta_key' => 'description',
-				'st_value' => '',
-				'prefix'   => '',
-				'suffix'   => '',
-				'escape'   => 'default',
-				'limit'    => 0,
-			),
+
 		);
 	}
 }
